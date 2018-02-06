068002b27f40abc6142132333b43dc4ce3cff636
Marcelo Perretta <marcelo.perretta@gmail.com>
2018-02-06 18:21:19 -0300

    First commit

---
+++ Google-DFP-Carthage/Google_DFP_Carthage.h
@@ -0,0 +1,19 @@
+//
+//  Google_DFP_Carthage.h
+//  Google-DFP-Carthage
+//
+//  Created by Marcelo Perretta on 06/02/2018.
+//  Copyright © 2018 MAWAPE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Google_DFP_Carthage.
+FOUNDATION_EXPORT double Google_DFP_CarthageVersionNumber;
+
+//! Project version string for Google_DFP_Carthage.
+FOUNDATION_EXPORT const unsigned char Google_DFP_CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Google_DFP_Carthage/PublicHeader.h>
+
+
